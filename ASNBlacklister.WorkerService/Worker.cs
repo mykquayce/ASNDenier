c73341700c18420bd8fa192089a86b7adbe053d8
Bob H <myk_quayce@hotmail.com>
2021-01-31 00:19:32 +0000

    adding workflowcore

--- ASNBlacklister.WorkerService/Worker.cs
+++ ASNBlacklister.WorkerService/Worker.cs
@@ -1,29 +1,39 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using WorkflowCore.Interface;
 
 namespace ASNBlacklister.WorkerService
 {
 	public class Worker : BackgroundService
 	{
 		private readonly ILogger<Worker> _logger;
+		private readonly IWorkflowHost _workflowHost;
 
-		public Worker(ILogger<Worker> logger)
+		public Worker(ILogger<Worker> logger, IWorkflowHost workflowHost)
 		{
-			_logger = logger;
+			_logger = logger
+				?? throw new ArgumentNullException(nameof(logger));
+
+			_workflowHost = workflowHost
+				?? throw new ArgumentNullException(nameof(workflowHost));
+
+			_workflowHost.RegisterWorkflow<Workflows.Workflow>();
 		}
 
 		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
 		{
+			await _workflowHost.StartAsync(stoppingToken);
+
 			while (!stoppingToken.IsCancellationRequested)
 			{
-				_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+				await _workflowHost.StartWorkflow(nameof(Workflows.Workflow));
 				await Task.Delay(1000, stoppingToken);
 			}
+
+			await _workflowHost.StopAsync(stoppingToken);
 		}
 	}
 }
