002c1762ac50c630986eebb40f6becc3860c1432
Bob H <myk_quayce@hotmail.com>
2021-01-31 00:19:32 +0000

    adding workflowcore

---
+++ ASNBlacklister.Workflows/Workflow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using WorkflowCore.Interface;
+
+namespace ASNDenier.Workflows
+{
+	public class Workflow : IWorkflow
+	{
+		public string Id => nameof(Workflow);
+		public int Version => 1;
+
+		public void Build(IWorkflowBuilder<object> builder)
+		{
+			builder
+				.StartWith<Steps.EchoStep>()
+					.Input(step => step.Message, _ => $"Worker running at: {DateTimeOffset.Now}")
+				.EndWorkflow();
+		}
+	}
+}
