33b66683fbaf1d6cec879f5c9dbb47808ede94a0
Bob H <myk_quayce@hotmail.com>
2021-02-16 03:32:51 +0000

    host tests

---
+++ ASNBlacklister.WorkerService.Tests/Fixtures/HostFixture.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+
+namespace ASNDenier.WorkerService.Tests.Fixtures
+{
+	public sealed class HostFixture : IDisposable
+	{
+		public HostFixture()
+		{
+			var hostBuilder = Program.CreateHostBuilder();
+			Host = hostBuilder.Build();
+		}
+
+		public void Dispose() => Host.Dispose();
+
+		public IHost Host { get; }
+	}
+}
