6d44af0d597203e246aeed5fb7f6915229ee1d05
Bob H <mail@bob.codes>
2021-04-24 11:05:17 +0100

    adding ClearBlacklistStep; removing EchoStep

---
+++ ASNBlacklister.Workflows/Steps/ClearBlacklistStep.cs
@@ -0,0 +1,24 @@
+﻿using Dawn;
+using System.Threading.Tasks;
+using WorkflowCore.Interface;
+using WorkflowCore.Models;
+
+namespace ASNBlacklister.Workflows.Steps
+{
+	public class ClearBlacklistStep : IStepBody
+	{
+		private readonly Helpers.SSH.Services.ISSHService _sshService;
+
+		public ClearBlacklistStep(Helpers.SSH.Services.ISSHService sshService)
+		{
+			_sshService = Guard.Argument(() => sshService).NotNull().Value;
+		}
+
+		public async Task<ExecutionResult> RunAsync(IStepExecutionContext context)
+		{
+			await _sshService.DeleteBlackholesAsync();
+
+			return ExecutionResult.Next();
+		}
+	}
+}
--- ASNBlacklister.Workflows/Workflow.cs
+++ ASNBlacklister.Workflows/Workflow.cs
@@ -11,15 +11,12 @@ namespace ASNBlacklister.Workflows
 		public void Build(IWorkflowBuilder<Models.PersistenceData> builder)
 		{
 			builder
-				.StartWith<Steps.EchoStep>()
-					.Input(step => step.Message, _ => $"Worker running at: {DateTimeOffset.Now}")
+				.StartWith<Steps.ClearBlacklistStep>()
 				.Then<Steps.GetASNNumbersStep>()
 					.Output(data => data.ASNNumbers, step => step.ASNNumbers)
 				.ForEach(data => data.ASNNumbers)
 					.Do(each => each
-						.StartWith<Steps.EchoStep>()
-							.Input(step => step.Message, (_, context) => (context.Item as int? ?? 0).ToString())
-						.Then<Steps.GetSubnetsStep>()
+						.StartWith<Steps.GetSubnetsStep>()
 							.Input(step => step.ASNNumber, (_, context) => context.Item as int? ?? 0)
 							.Output(data => data.SubnetAddresses, step => step.Subnets)
 						.Then<Steps.BlacklistSubnetsStep>()
