1196fe19a36d85ee7eb573253bc480212e61db40
Bob H <mail@bob.codes>
2021-05-16 12:20:21 +0100

    GetBlacklistedASNs test

--- ASNBlacklister.Services.Tests/UnitTest1.cs
+++ ASNBlacklister.Services.Tests/UnitTest1.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -15,7 +18,9 @@ namespace ASNDenier.Services.Tests
 			_sshService = fixture.SSHService;
 		}
 
-		[Theory]
+#pragma warning disable xUnit1004 // Test methods should not be skipped
+		[Theory(Skip = "desctructive")]
+#pragma warning restore xUnit1004 // Test methods should not be skipped
 		[InlineData(32_934)] // facebook
 		[InlineData(7_224)] // AMAZON - AS, US
 		[InlineData(8_987)] // AMAZON EXPANSION, IE
@@ -30,6 +35,45 @@ namespace ASNDenier.Services.Tests
 			Assert.DoesNotContain(default, subnets);
 
 			await _sshService.AddBlackholesAsync(subnets);
+		}
+
+		[Fact]
+		public async Task GetBlacklistedASNs()
+		{
+			var cache = new Dictionary<IPAddress, (int, string)>();
+			//ssh into router
+			//	ip route show | grep ^blackhole | awk '{print($2)}'
+			var blackholes = _sshService.GetBlackholesAsync();
+			//foreach ip
+			await foreach (var (ip, _) in blackholes)
+			{
+				//	check cache
+				//	tryget ip
+				if (cache.TryGetValue(ip, out var _)) continue;
+				//	false
+				//	tcp client
+				//		111.111.111.111\n
+				var details = _whoIsClient.GetWhoIsDetailsAsync(ip);
+				//		returns asns
+				await foreach (var (_, asn, description, _) in details)
+				{
+					//		-F -K -i 11111\n
+					var ips = _whoIsClient.GetIpsAsync(asn);
+					//		returns ips
+					//			foreach ip
+					await foreach (var (ip2, _) in ips)
+					{
+						//				cache
+						//				ip=asn
+						cache.TryAdd(ip2, (asn, description));
+					}
+				}
+			}
+
+			var asns = cache.Values
+				.Distinct()
+				.OrderBy(t => t.Item2, StringComparer.InvariantCultureIgnoreCase)
+				.ToList();
 		}
 	}
 }
