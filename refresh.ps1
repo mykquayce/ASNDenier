994e801627b66934c0d4705a249877be1f5e7768
bob h <mail@bob.codes>
2022-07-29 00:40:46 +0100

    refresh script

---
+++ refresh.ps1
@@ -0,0 +1,19 @@
+﻿docker pull mcr.microsoft.com/dotnet/runtime:6.0
+docker pull mcr.microsoft.com/dotnet/sdk:6.0
+docker pull eassbhhtgu/asnblacklister:latest
+
+$base1 = docker image inspect --format '{{.Created}}' mcr.microsoft.com/dotnet/runtime:6.0
+$base2 = docker image inspect --format '{{.Created}}' mcr.microsoft.com/dotnet/sdk:6.0
+$target = docker image inspect --format '{{.Created}}' eassbhhtgu/asnblacklister:latest
+
+if ($base1 -gt $target -or $base2 -gt $target) {
+
+	docker build `
+		--secret id=ca_crt,src=${env:userprofile}\.aspnet\https\ca.crt `
+		--tag eassbhhtgu/asnblacklister:latest `
+		.
+	if (!$?) { exit 1; }
+
+	docker push eassbhhtgu/asnblacklister:latest
+	if (!$?) { exit 1; }
+}
