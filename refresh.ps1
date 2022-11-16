34bed32e8375825e3ce17a1117615ce9ee6d6c79
bob h <mail@bob.codes>
2022-11-16 13:51:14 +0000

    fixing/improving the refresh script

--- refresh.ps1
+++ refresh.ps1
@@ -1,12 +1,21 @@
-﻿docker pull mcr.microsoft.com/dotnet/runtime:7.0
+﻿if ( git status --short ) {
+	echo "pending changes"
+	exit 0
+}
+
+docker pull mcr.microsoft.com/dotnet/runtime:7.0
 docker pull mcr.microsoft.com/dotnet/sdk:7.0
 docker pull eassbhhtgu/asndenier:latest
 
-$base1 = docker image inspect --format '{{.Created}}' mcr.microsoft.com/dotnet/runtime:6.0
-$base2 = docker image inspect --format '{{.Created}}' mcr.microsoft.com/dotnet/sdk:6.0
+$base1 = docker image inspect --format '{{.Created}}' mcr.microsoft.com/dotnet/runtime:7.0
+$base2 = docker image inspect --format '{{.Created}}' mcr.microsoft.com/dotnet/sdk:7.0
 $target = docker image inspect --format '{{.Created}}' eassbhhtgu/asndenier:latest
 
 if ($base1 -gt $target -or $base2 -gt $target) {
+
+	git clean -dfx
+	git fetch
+	git pull
 
 	docker build `
 		--secret id=ca_crt,src=${env:userprofile}\.aspnet\https\ca.crt `
