1d4acb2d42e8014d9ebb18d94b8040d02e540712
bob h <mail@bob.codes>
2024-07-28 09:09:24 +0100

    tidying config

--- ASNDenier.WorkerService/Program.cs
+++ ASNDenier.WorkerService/Program.cs
@@ -13,14 +13,23 @@ builder
 			.AddTransient<Helpers.Networking.Clients.IWhoIsClient, Helpers.Networking.Clients.Concrete.WhoIsClient>()
 			.AddSshClient(b =>
 			{
-				b.Host = hostContext.Configuration["router:host"] ?? throw new Exception();
-				b.Port = ushort.TryParse(hostContext.Configuration["router:port"], out var u) ? u : throw new Exception();
-				b.Username = hostContext.Configuration["router:username"] ?? throw new Exception();
-				b.Password = hostContext.Configuration["router:password"];
-				b.PathToPrivateKey = hostContext.Configuration["router:pathtoprivatekey"];
+				var config = hostContext.Configuration.GetSection("router").Get<Config>();
+
+				if (string.IsNullOrEmpty(config.Host) || string.IsNullOrEmpty(config.Username))
+				{
+					throw new KeyNotFoundException("router config missing");
+				}
+
+				b.Host = config.Host;
+				b.Port = config.Port;
+				b.Username = config.Username;
+				b.Password = config.Password;
+				b.PathToPrivateKey = config.PathToPrivateKey;
 			});
 	});
 
 IHost host = builder.Build();
 
 host.Run();
+
+internal readonly record struct Config(string Host, ushort Port, string Username, string? Password, string? PathToPrivateKey);
