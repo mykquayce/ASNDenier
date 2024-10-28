e30a2f4977d16452199c82825f5678728429df18
bob h <mail@bob.codes>
2024-10-28 22:09:09 +0000

    tidying and library update (helpers.ssh)

--- ASNDenier.WorkerService/Worker.cs
+++ ASNDenier.WorkerService/Worker.cs
@@ -1,58 +1,48 @@
-using Dawn;
 using Helpers.Timing;
 using Microsoft.Extensions.Options;
 
 namespace ASNDenier.WorkerService;
 
-public class Worker : BackgroundService
+public class Worker(
+	ILogger<Worker> logger,
+	IOptions<Models.ASNNumbers> asnNumbersOptions,
+	IOptions<Interval> intervalOptions,
+	Helpers.SSH.IService sshService,
+	Helpers.Networking.Clients.IWhoIsClient whoIsClient) : BackgroundService
 {
-	private readonly ILogger<Worker> _logger;
-	private readonly IReadOnlyDictionary<string, int[]> _asnNumbers;
-	private readonly IInterval _interval;
-	private readonly Helpers.SSH.IService _sshService;
-	private readonly Helpers.Networking.Clients.IWhoIsClient _whoIsClient;
-
-	public Worker(
-		ILogger<Worker> logger,
-		IOptions<Models.ASNNumbers> asnNumbersOptions,
-		IOptions<Interval> intervalOptions,
-		Helpers.SSH.IService sshService,
-		Helpers.Networking.Clients.IWhoIsClient whoIsClient)
-	{
-		_logger = Guard.Argument(logger).NotNull().Value;
-		_asnNumbers = Guard.Argument(asnNumbersOptions).NotNull().Wrap(o => o.Value)
-			.NotNull().NotEmpty().Value;
-		_interval = Guard.Argument(intervalOptions).NotNull().Wrap(o => o.Value)
-			.NotNull().Value;
-		_sshService = Guard.Argument(sshService).NotNull().Value;
-		_whoIsClient = Guard.Argument(whoIsClient).NotNull().Value;
-	}
+	private readonly IReadOnlyDictionary<string, int[]> _asnNumbers = asnNumbersOptions.Value;
+	private readonly IInterval _interval = intervalOptions.Value;
 
 	protected override async Task ExecuteAsync(CancellationToken stoppingToken)
 	{
 		while (!stoppingToken.IsCancellationRequested)
 		{
-			await _sshService.DeleteBlackholesAsync();
-			_logger.LogInformation("deleted blackholes");
+			await sshService.DeleteBlackholesAsync(stoppingToken);
+			logger.LogInformation("deleted blackholes");
 
 			foreach (var (organization, asns) in _asnNumbers)
 			{
-				_logger.LogInformation("{organization} has {count} asn(s) : {asns}", organization, asns.Length, string.Join(", ", asns));
+				logger.LogInformation("{organization} has {count} asn(s) : {asns}", organization, asns.Length, string.Join(", ", asns));
 
 				foreach (var asn in asns)
 				{
-					var prefixes = await _whoIsClient.GetIpsAsync(asn, stoppingToken).ToListAsync(stoppingToken);
+					var prefixes = await whoIsClient.GetIpsAsync(asn, stoppingToken).ToArrayAsync(stoppingToken);
 
-					_logger.LogInformation("Applying {Count} prefix(es)", prefixes.Count);
+					logger.LogInformation("Applying {Count} prefix(es)", prefixes.Length);
 
-					await _sshService.AddBlackholesAsync(prefixes);
+					var idx = 1;
+					foreach (var batch in prefixes.Chunk(size: 100))
+					{
+						logger.LogInformation("batch {idx}", idx++);
+						await sshService.AddBlackholesAsync(batch, stoppingToken);
+					}
 				}
 			}
 
 			var delay = new TimeSpan(long.Max(
 				TimeSpan.FromMinutes(5).Ticks,
 				(_interval.Next() - DateTime.UtcNow).Ticks));
-			_logger.LogInformation(@"sleeping for {minutes:hh\:mm\:ss}", delay);
+			logger.LogInformation(@"sleeping for {minutes:hh\:mm\:ss}", delay);
 			await Task.Delay(delay, stoppingToken);
 		}
 	}
